392e91688d98b14e7d997e5f76ad0eb57a8dfe2c
Anand-1812 <anand.work2005@gmail.com>
2025-05-31 21:04:48 +0530

    que-78 (subset)

--- recursion/78.py
+++ recursion/78.py
@@ -4,14 +4,14 @@ nums = [1, 2, 3]
 
 def subsets(nums: List[int]) -> List[List[int]]:
     n = len(nums)
-    subset_count = 1 << n  # 2^n subsets
+    subset_count = 1 << n
     ans = []
 
     for i in range(subset_count):
         current = []
         for j in range(n):
             if i & (1 << j):
-                current.append(nums[j])  # Corrected from nums[i] to nums[j]
+                current.append(nums[j])
         ans.append(current)
 
     return ans
